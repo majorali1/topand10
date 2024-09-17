6c9827577249b96d91aff454dabcb5de9fa92abf
sirajali <sirajalipyt@gmail.com>
2024-09-17 14:15:34 +0500

    fixed all bugs and continue development

---
+++ backend/core/config.py
@@ -0,0 +1,23 @@
+
+
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    PROJECT_NAME:str = "Topand10"
+    PROJECT_VERSION:str =  "1.0.0"
+
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    DATABASE_URL =  "sqlite:///./sql_app.db"
+
+
+settings = Settings()
